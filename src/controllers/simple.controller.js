b23a94698109e37680846902d467ecf272442f9c
Anuj Modi <anujmodi47@gmail.com>
2023-09-19 10:53:47 +0530

    first commit for dev branch

--- src/controllers/simple.controller.js
+++ src/controllers/simple.controller.js
@@ -1,1 +1,3 @@
 // this is controller and i am pushing code to main branch
+
+// write something here and i will push this code to second new branch
--- src/routes/simple.route.js
+++ src/routes/simple.route.js
@@ -1,1 +1,3 @@
 // route file and push code for main branch
+
+// second branch code here need to push somewhere
